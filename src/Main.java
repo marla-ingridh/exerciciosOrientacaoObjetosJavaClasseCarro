d9bfa92a71a14d6d4d704db8c475b5952dc83749
Marla Ingridh <marla.ingridh@gmail.com>
2024-01-30 12:01:07 -0400

    Primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        ModeloCarro carro1 = new ModeloCarro();
+        carro1.setNome("Cobalt");
+        carro1.setPrecoAno1(40000);
+        carro1.setPrecoAno2(34000);
+        carro1.setPrecoAno3(90000);
+        carro1.exibirInformacoes();
+
+    }
+}
